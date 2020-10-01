2b9e476690adbc5e8575cfd62fc47cf1caced030
Saha <aveek.s98@gmail.com>
2020-10-01 10:15:02 +0530

    Remove test

--- DataStructures.js
+++ DataStructures.js
@@ -1177,5 +1177,5 @@ function BinarySearch(arr, l, r, key) {
         return -1
 }
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(BinarySearch(arr, 0, arr.length, 6))
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(BinarySearch(arr, 0, arr.length, 6))
